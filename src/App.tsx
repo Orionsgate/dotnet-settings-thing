0e65edb20f0892d8df5c29e7db040492553498cd
Nathan <19893502+Orionsgate@users.noreply.github.com>
2026-02-13 18:28:04 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <main className="mx-auto w-full max-w-6xl px-4 py-10 sm:px-6 lg:px-8" />
+    </div>
+  )
+}
+
+export default App
